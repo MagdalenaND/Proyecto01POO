62d230849f5f7da4d63ddf5b99bf5b52f96c95ee
Magdalena Antonia Núñez Duarte <manunez25@alumnos.utalca.cl>
2026-05-20 16:00:03 -0400

    Add files via upload
    Se agregó método get y toString

--- src/main/java/cl/utalca/InventarioLetras.java
+++ src/main/java/cl/utalca/InventarioLetras.java
@@ -3,7 +3,7 @@ package cl.utalca;
 
 public class InventarioLetras {
 
-    private int [] counts; // Arreglo que guarda las veces que aparece cada letra del abcdario
+    private int [] counts; // Arreglo que guarda las veces que aparece cada letra del abecedario
     private int totalCount; //Guarda el total de letras encontradas
     private int nonZeroCount; // Guarda la cantidad de letras distintas que hay
 
@@ -12,30 +12,52 @@ public class InventarioLetras {
         totalCount = 0;  //Inicializadores
         nonZeroCount = 0;
 
-        data = data.toLowerCase(); //convierte a minúscula
+        data = data.toLowerCase(); //Convierte a minúscula
 
         for(int i = 0; i < data.length(); i++){ // Recorre cada carácter
 
             char letra = data.charAt(i);
 
-            if(letra>= 'a' && letra <= 'z'){ // ve que la letra esté dentro de a-z
-                int indice = letra - 'a'; // uso de ascii para las posiciones, para que no sea largo
+            if(letra>= 'a' && letra <= 'z'){ // Ve que la letra esté dentro de a-z
+                int indice = letra - 'a'; // Uso ASCII para obtener la posición de la letra en el arreglo
 
                 if(counts[indice] == 0){ // si aparece por 1ª vez la letra se agrega al contador
                     nonZeroCount++;      // de letras distintas
                 }
-                counts[indice]++; // aumenta la cantidad de esa letra
+                counts[indice]++; // Aumenta la cantidad de esa letra
 
-                totalCount++; // aumenta total de letras
+                totalCount++; // Aumenta total de letras
             }
         }
     }
 
-    public int size (){ //metodo que retorna el total de letras que se encontraron
+    public int size (){ //Metodo que retorna el total de letras que se encontraron
         return totalCount;
     }
-    public boolean isEmpty (){ // metodo que retorna "true" si esta vacio el inventario
+    public boolean isEmpty (){ // Metodo que retorna "true" si esta vacio el inventario
         return nonZeroCount == 0;
+    }
+    public int get(char letra){
+        letra = Character.toLowerCase(letra); //Convierte a minuscula
+        if (letra< 'a' || letra> 'z'){
+            throw new IllegalArgumentException("El carácter no es válido"); //Validacion
+        }
+        return counts[letra - 'a']; // Retorna cuantas veces aparece la letra
+    }
+
+    public String toString() {
+        String resultado = "[";
+
+        for(int i = 0; i < 26; i++){ // Recorre las 26 letras
+
+            for(int j = 0; j < counts[i]; j++){// Repite la letra según su cantidad
+
+                resultado += (char)('a' + i); // Genera letras
+            }
+        }
+        resultado += "]";
+
+        return resultado;
     }
 }
 
--- src/main/java/cl/utalca/Main.java
+++ src/main/java/cl/utalca/Main.java
@@ -1,11 +1,12 @@
 package cl.utalca;
 
-public class Main { // primera prueba de metodo size y isEmpty con "Hola Mundo"
+public class Main { // Prueba de metodo get y toString con "Hola Mundo"
     public static void main ( String [] args ) {
-        InventarioLetras inv = new InventarioLetras (" Hola Mundo "); // se crea objeto InventarioLetras
+        InventarioLetras inv = new InventarioLetras (" Hola Mundo "); // Se crea objeto InventarioLetras
         System.out.println (" size : " + inv.size()); // 9
         System.out.println (" isEmpty : " + inv.isEmpty()); // false
-
+        System.out.println(inv.get('o')); // Debe dar 2
+        System.out.println(inv); // Tiene que retornar [adhlmnoou]
 
     }
 }
